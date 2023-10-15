09a0b516691470c75023aa0871c4c5f378ba9679
SMSaeedi <mahsa.saeedy@gmail.com>
2023-10-15 19:03:19 +0200

    Changed the algorithm

--- src/main/java/codility/algorithm/examples/FindHillsAndValleys.java
+++ src/main/java/codility/algorithm/examples/FindHillsAndValleys.java
@@ -4,12 +4,8 @@ import java.util.Arrays;
 
 public class FindHillsAndValleys {
     public static int hillsAndValleys(int[] arr) {
-        int counter = 0;
-        for (Integer i : arr) {
-            if (i.equals(arr[0]))
-                counter++;
-        }
-        if (counter == arr.length)
+        var match = Arrays.stream(arr).allMatch(s -> s == (arr[0]));
+        if (match)
             return 0;
 
         var min = Arrays.stream(arr)
@@ -25,20 +21,58 @@ public class FindHillsAndValleys {
         var fMaxCount = Arrays.stream(arr)
                 .filter(i -> i == fMax)
                 .count();
+
+        Long response = 0L;
+
+        if (isArrayIncreasing(arr))
+            return (int) minCount;
+        if (isArrayDecreasing(arr))
+            return (int) fMaxCount;
+
         var sMaxCount = Arrays.stream(arr)
                 .filter(value -> value == fMax - 1)
                 .count();
+        response = minCount + fMaxCount + sMaxCount;
 
-        Long res = minCount + fMaxCount + sMaxCount;
-        return res.intValue();
+        return response.intValue();
 
     }
 
+    static boolean isArrayIncreasing(int[] array) {
+        boolean result = true;
+        int i;
+        for (i = 0; i < array.length - 1; i++) {
+            if (array[i] >= array[i + 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    static boolean isArrayDecreasing(int[] array) {
+        boolean result = true;
+        int i;
+        for (i = 0; i < array.length - 1; i++) {
+            if (array[i] <= array[i + 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {2, 2, 3, 4, 3, 3, 2, 2, 1, 1, 2, 5};
+        int[] arr = {2, 2, 3, 4, 3, 3, 2, 2, 1, 1, 2, 5, 1, 1, 6};
         System.out.print(hillsAndValleys(arr));
 
         int[] arr1 = {-3, -3};
         System.out.print(hillsAndValleys(arr1));
+
+        int[] arr2 = {1, 2, 3};
+        System.out.print(hillsAndValleys(arr2));
+
+        int[] arr3 = {3, 2, 1};
+        System.out.print(hillsAndValleys(arr3));
     }
 }
