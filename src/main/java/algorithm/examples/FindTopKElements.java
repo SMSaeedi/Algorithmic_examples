74441da67f2db3e5410e6ea1c8d5f0c8b4dc16f1
SMSaeedi <mahsa.saeedy@gmail.com>
2024-07-24 15:04:17 +0200

    using diff approach for finding the top K element

--- src/main/java/algorithm/examples/FindTopKElements.java
+++ src/main/java/algorithm/examples/FindTopKElements.java
@@ -8,12 +8,17 @@ import java.util.stream.Collectors;
 
 public class FindTopKElements {
     public static void main(String[] args) {
-        System.out.println(FindTopKElements.findTopKElementUsingHeapOrderedTreeSet(List.of(1, 2, 3, 3, 4, 4, 5), 2));
+        System.out.println(FindTopKElements.topKElementUsingTreeSet(List.of(1, 2, 3, 3, 4, 4, 5), 2));
+        System.out.println(FindTopKElements.topKElementUsingStream(List.of(1, 2, 3, 3, 4, 4, 5, 6, 6, 7, 7, 8, 9), 3));
     }
 
-    public static List<Integer> findTopKElementUsingHeapOrderedTreeSet(List<Integer> lst, int k) {
+    public static List<Integer> topKElementUsingTreeSet(List<Integer> list, int k) {
         Set<Integer> sortedSet = new TreeSet<>(Comparator.reverseOrder());
-        sortedSet.addAll(lst);
+        sortedSet.addAll(list);
         return sortedSet.stream().limit(k).collect(Collectors.toList());
+    }
+
+    public static List<Integer> topKElementUsingStream(List<Integer> list, int k) {
+        return list.stream().sorted(Comparator.reverseOrder()).distinct().limit(k).toList();
     }
 }
